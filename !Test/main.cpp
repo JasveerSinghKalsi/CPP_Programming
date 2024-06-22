779112058842d9be7013c6bc81220baff179c00e
Jasveer <kalsijasveer2003@gmail.com>
2024-06-22 11:47:30 +0530

    Added main.c

---
+++ !Test/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <limits>
+
+int main()
+{
+    std::cout << "Hello World!" << std::endl;
+    
+    return 0;
+}
