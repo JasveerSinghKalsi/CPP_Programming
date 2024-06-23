751d6b046e1ed82eff28de7f26f8ed6e82665cd9
Jasveer <kalsijasveer2003@gmail.com>
2024-06-23 16:48:07 +0530

    test

--- !Test/main.cpp
+++ !Test/main.cpp
@@ -1,13 +1,22 @@
-#include <iostream>
-#include <string>
-#include <cmath>
-#include <cstdlib>
-#include <ctime>
-#include <limits>
+#include <iostream> //For Inputs and Outputs
+#include <string> //For Strings and Arrays
+#include <array> //For STL Arrays
+#include <vector> //For Vectors
+#include <cmath> //For Mathematics functions
+#include <cstdlib> //For Dynamic Memory, Random Number
+#include <ctime> //For Date and Time
+#include <limits> //For Arithmetic characteristics
+#include <climits> //For defined Constants
 
 int main()
 {
-    std::cout << "Hello World!" << std::endl;
-    
+    int data[] = {1,2,3,4,5,6};
+
+    for (int n : data)
+    {
+        std::cout << n << "\t";
+    }
+    std::cout << "\n";
+        
     return 0;
 }
