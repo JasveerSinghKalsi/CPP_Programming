28c538da434a04b47d1909755190136589e5d2ef
Jasveer <kalsijasveer2003@gmail.com>
2024-06-25 01:21:22 +0530

    added rectangle area

--- Maths/math_stuff.cpp
+++ Maths/math_stuff.cpp
@@ -7,5 +7,12 @@ int main()
 	std::cout << utilz::pow(3, 3) << std::endl;
 	std::cout << utilz::pow(3) << std::endl;
 
+	Rectangle rect;
+	rect.length = 5;
+	rect.width = 7;
+	std::cout << utilz::area(rect.length, rect.width) << std::endl;
+	std::cout << utilz::area(rect.length) << std::endl;
+	std::cout << utilz::area(rect) << std::endl;
+
 	return 0;
 }
